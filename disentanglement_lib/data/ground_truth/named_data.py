f86d96a9e7657c560005e5ebc455b09b8a9c644f
simonbing <bings@student.ethz.ch>
2021-01-02 12:33:25 +0100

    Adds new shapes subsets

--- disentanglement_lib/data/ground_truth/named_data.py
+++ disentanglement_lib/data/ground_truth/named_data.py
@@ -275,5 +275,23 @@ def get_named_subset(name):
         subset_full = np.reshape(np.transpose(subset_full, (0, 2, 1)), (
         subset_shape[0] * subset_shape[2], subset_shape[1]))
         return subset_full
+    elif name == "shapes_part1":
+        subset_path = '/cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/shapes3d/factors_shapes_part1.npz'
+        subset = np.load(subset_path)
+        subset_full = np.concatenate(
+            (subset['factors_train'], subset['factors_test']))
+        subset_shape = subset_full.shape
+        subset_full = np.reshape(np.transpose(subset_full, (0, 2, 1)), (
+        subset_shape[0] * subset_shape[2], subset_shape[1]))
+        return subset_full
+    elif name == "shapes_part2":
+        subset_path = '/cluster/work/grlab/projects/projects2020_disentangled_gpvae/data/shapes3d/factors_shapes_part2.npz'
+        subset = np.load(subset_path)
+        subset_full = np.concatenate(
+            (subset['factors_train'], subset['factors_test']))
+        subset_shape = subset_full.shape
+        subset_full = np.reshape(np.transpose(subset_full, (0, 2, 1)), (
+        subset_shape[0] * subset_shape[2], subset_shape[1]))
+        return subset_full
     else:
         raise ValueError("Invalid subset name.")
